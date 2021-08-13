c146ba51c311c3010e4641341d76f85b007ecc5a
mcglasp <nmglasper@gmail.com>
2021-08-13 10:37:44 +0000

    First stages of deployment to Heroku: created Procfile and requirements

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ms4.wsgi:application
--- ms4/settings.py
+++ ms4/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jjt5h#cre*ed*%1^!04k9u1p7oat8b!1@yi-2o&p*h@z$ckcj_'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
