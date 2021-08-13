a9ed6da1b7bf080977fed16f14f77e0aa480bf11
mcglasp <nmglasper@gmail.com>
2021-08-13 11:21:33 +0000

    Updated allowed hosts

--- ms4/settings.py
+++ ms4/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ms4-big-idea-books.herokuapp.com', 'localhost']
 
 
 # Application definition
